de027d56d2e6183350398c3f5421631e781d7b46
edenworkroom <edenworkroom@163.com>
2020-04-28 01:03:05 +0800

    update

---
+++ docs/precache-manifest.04fca5486dc4ca0031751ef07cd0f67b.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "152c220a4d7c08d763a27c966d9ec51e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9dedda1290f118ed662a",
+    "url": "./static/css/2.690b9c2c.chunk.css"
+  },
+  {
+    "revision": "ba7800ac62c567b59e73",
+    "url": "./static/css/main.80c8cace.chunk.css"
+  },
+  {
+    "revision": "9dedda1290f118ed662a",
+    "url": "./static/js/2.d3123b92.chunk.js"
+  },
+  {
+    "revision": "d32eeb8ac1e79aba68eaa053a3aa8db6",
+    "url": "./static/js/2.d3123b92.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ba7800ac62c567b59e73",
+    "url": "./static/js/main.2041e8d7.chunk.js"
+  },
+  {
+    "revision": "5ab5aacceb7847d6c205",
+    "url": "./static/js/runtime-main.b25960d3.js"
+  },
+  {
+    "revision": "64fe8c862ee19974a3a518ff40044595",
+    "url": "./static/media/1.64fe8c86.png"
+  },
+  {
+    "revision": "4911c520411707d89cef7e7d9a545fff",
+    "url": "./static/media/AIPP.4911c520.png"
+  },
+  {
+    "revision": "c67377c5c0832fae9c410e4888a049ca",
+    "url": "./static/media/ASNOW.c67377c5.png"
+  },
+  {
+    "revision": "e9d408bd0f5a858a11f33cbdf14b892f",
+    "url": "./static/media/FFFF.e9d408bd.png"
+  },
+  {
+    "revision": "af3a347311df3ce4f0bb41f11dd922db",
+    "url": "./static/media/HAPY.af3a3473.png"
+  },
+  {
+    "revision": "9635f74b9ee84c9059d98625582526a8",
+    "url": "./static/media/NEWBITS.9635f74b.png"
+  },
+  {
+    "revision": "c15d08c10ddf693ecd096fc02be6bce7",
+    "url": "./static/media/ORIGO.c15d08c1.png"
+  },
+  {
+    "revision": "e9d408bd0f5a858a11f33cbdf14b892f",
+    "url": "./static/media/SERO.e9d408bd.png"
+  },
+  {
+    "revision": "ecfe7e617ee454141c50930dbcabf00b",
+    "url": "./static/media/SVVC.ecfe7e61.png"
+  },
+  {
+    "revision": "faf7618d93de2fe4a8e4b929b2429073",
+    "url": "./static/media/THE_FIRST_PRIVACY_COIN.faf7618d.png"
+  },
+  {
+    "revision": "e9d408bd0f5a858a11f33cbdf14b892f",
+    "url": "./static/media/TTTT.e9d408bd.png"
+  },
+  {
+    "revision": "8f0c6e6f5ecbdd8f4402473cc6cd9eaf",
+    "url": "./static/media/USDS.8f0c6e6f.png"
+  },
+  {
+    "revision": "40a2c7527e0873c9b11a9e9950e6d752",
+    "url": "./static/media/VIRTUE.40a2c752.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "./static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "./static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "./static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "./static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "./static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "./static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "./static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "./static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "./static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "./static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "./static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "acb9934dae87e32f2f4d9c29294f9028",
+    "url": "./static/media/logo.acb9934d.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "./static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "./static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "./static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "./static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "./static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
